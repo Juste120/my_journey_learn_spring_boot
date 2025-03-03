0f3aa392e8cd22c1ed7dbb14e7167e55ca130aea
PAKOU Komi Juste <Juste120@users.noreply.github.com>
2025-03-03 08:16:35 +0000

    use the test unit and the api to pathvariable request ,connect to H2 database

--- cashcard/src/test/java/example/cashcard/CashCardApplicationTests.java
+++ cashcard/src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,20 +1,57 @@
 package example.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.core.io.ClassPathResource;
 
-@JsonTest
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/*@JsonTest*/
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
 	@Autowired
-	private JacksonTester<CashCard> json;
+	private TestRestTemplate restTemplate;
+
+
+	//use the test to the getById methode to cashcardController
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isEqualTo(99);
+
+		Double amount = documentContext.read("$.amount");
+		assertThat(amount).isEqualTo(123.45);
+	}
+
+
+
+
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+
+	/*private JacksonTester<CashCard> json;
 
 	@Test
 	void cashCardSerializationTest() throws IOException {
@@ -41,6 +78,6 @@ class CashCardApplicationTests {
 				.isEqualTo(new CashCard(99L, 123.45));
 		assertThat(json.parseObject(expected).id()).isEqualTo(99);
 		assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
-	}
+	}*/
 
 }
